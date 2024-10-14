ce7be7392caea99c8df705039d85942b343cff2a
Nadifo Ismail <nadifoismail80@gmail.com>
2024-10-14 11:52:58 +0300

    modifie index.js index.css

--- src/index.js
+++ src/index.js
@@ -1,30 +1,42 @@
 //fetch all movie titles
 fetch("http://localhost:3000/films")
   .then((res) => res.json())
-  .then((data) => {
-    const films = document.getElementById("films");
+  .then((movies) => {
+    movies.forEach((movie) => {
+      const films = document.getElementById("films");
+      const list = document.createElement("li");
+      list.textContent = movie.title;
+      list.classList.add("film", "item","movielist")
+      if(movie.tickets_sold === movie.capacity){
+        list.classList.add("sold-out")
+      }
 
-    for (post of data) {
-      films.innerHTML += `
-        <ul class="list-group list-group-flush">
+       const deleteButton = document.createElement("button")
+       deleteButton.classList.add("col" ,"deletebtn")
+       deleteButton.textContent = "delete";
 
-        <li class="list-group-item" >${post.title} </li> 
-
-        </ul>`;
+       list.appendChild(deleteButton)
+       
+        deleteButton.addEventListener("click" ,(e) => {
+          deleteButton.parentElement.remove()
+          deleteFilm(movie);
+        })
+        films.appendChild(list)
+    })
     }
-  });
-
+  );
+  
 //showing info
 const showing = document.getElementById("showing");
-showing.addEventListener("click",(e) => {
-e.preventDefault()
-
-})
+showing.addEventListener("click", (e) => {
+  e.preventDefault();
+});
 
 fetch("http://localhost:3000/films/1")
   .then((res) => res.json())
   .then((movie) => {
     const poster = document.getElementById("poster");
+
 
     poster.src = movie.poster;
 
@@ -42,16 +54,22 @@ fetch("http://localhost:3000/films/1")
               </div>
             </div>
             <div class="extra content">
-              <button onclick="buyButton(${
-                movie.id
-              })"id="buy-ticket" class="ui orange button">
-                Buy Ticket
+              <button id="buy-ticket" class="ui orange button">
+                ${movie.tickets_sold == movie.capacity ? "Sold out":"Buy ticket" }
               </button>
             </div>
           </div>
         </div>
 
         `;
+
+        document.getElementById("buy-ticket").addEventListener("click", (e) => {
+          e.preventDefault();
+       
+          buyButton(movie.id);
+          postTicket(movie.id);
+
+        })
 
   });
 
@@ -60,39 +78,59 @@ fetch("http://localhost:3000/films/1")
 function buyButton(id) {
 
   const buyTicket = document.getElementById("buy-ticket");
-  buyTicket.addEventListener("click",(e) => {
-    e.preventDefault()
-
+ 
     fetch(`http://localhost:3000/films/${id}`)
-  .then((response) => response.json())
-  .then((movie) => {
+      .then((response) => response.json())
+      .then((movie) => {
+        if (movie.tickets_sold < movie.capacity) {
+          movie.tickets_sold += 1;
 
-
-    if(movie.tickets_sold < movie.capacity){
-      movie.tickets_sold += 1;
-    }
-    else{
-      
-    }
-
-    fetch(`http://localhost:3000/films/${id}`, {
-      method: "PATCH",
-      body: JSON.stringify({
-        tickets_sold: movie.tickets_sold 
-      }),
-      headers: {
-        "Content-type": "application/json; charset=UTF-8",
-      },
-    })
-      .then((res) => res.json())
-      .then(() => {
-        console.log("Ticket purchased successfully")
+          fetch(`http://localhost:3000/films/${id}`, {
+            method: "PATCH",
+            body: JSON.stringify({
+              tickets_sold: movie.tickets_sold,
+            }),
+            headers: {
+              "Content-type": "application/json; charset=UTF-8",
+            },
+          })
+            .then((res) => res.json())
+            .then(() => {
+              const numTickets = movie.capacity - movie.tickets_sold;
+              if (movie.tickets_sold === movie.capacity) {
+                buyTicket.textContent = "Sold out";
+                buyTicket.disabled = true;
+              }
+            });
+        }
       });
-  });
-
-    
+}
+//delete film
+function deleteFilm(movie){
+  fetch(`http://localhost:3000/films/${movie.id}`, {
+    method: "DELETE",
   })
+  .then((res) => res.json())
+  .then(() => {
 
-  
+    console.log("Movie deleted")
+  }) 
+}
+
+//post film
+function postTicket (movie_id) {
+  fetch('http://localhost:3000/tickets', {
+
+    method: 'POST',
+    body: JSON.stringify({
+      "film_id": movie_id,
+      "number_of_tickets": 1
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => console.log(json));
   
 }
